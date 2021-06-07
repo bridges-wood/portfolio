3a6d41577b28e51343b15ec9f0ce7ec80c65a298
Max Wood <bridges.wood@gmail.com>
2021-06-07 22:15:06 +0100

    Fix project carousel orientation.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,20 +1,51 @@
 import FeaturedProjectPreview from '@components/FeaturedProjectPreview'
 import Project from '@components/Project'
+import useWindowDimensions from '@hooks/useWindowDimensions'
 import featuredProjects from '@json/featuredProjects'
 import { GithubProject } from '@typings/api/Projects'
 import fetchProjects from '@utils/repos'
 import { GetStaticProps } from 'next'
-import React from 'react'
-import Slider from 'react-slick'
+import React, { useEffect, useRef, useState } from 'react'
+import Slider, { Settings as SliderProps } from 'react-slick'
 
 interface PageProps {
 	projects: GithubProject[]
 	featured: any[]
 }
 
-// TODO change orientation page in landscape mode
+// TODO add buttons in vertical orientation.
 
 const Projects = ({ projects, featured }: PageProps) => {
+	const { width } = useWindowDimensions()
+	const [sliderProps, setSliderProps] = useState<SliderProps>({})
+	const sliderRef = useRef<Slider>()
+
+	useEffect(() => {
+		const commonProps: Partial<SliderProps> = {
+			infinite: true,
+			speed: 800,
+			autoplay: true,
+			autoplaySpeed: 5000,
+			onSwipe: () => {
+				sliderRef.current.slickPause()
+			},
+		}
+		if (width > 800) {
+			setSliderProps({
+				...commonProps,
+				arrows: false,
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				vertical: true,
+				verticalSwiping: true,
+			})
+		} else {
+			setSliderProps({
+				...commonProps,
+			})
+		}
+	}, [width])
+
 	return (
 		<div>
 			<h2>Projects</h2>
@@ -22,12 +53,12 @@ const Projects = ({ projects, featured }: PageProps) => {
 				A sample of some of the projects that I've worked on for personal
 				projects, assignments and on freelance.
 			</p>
-			<div className='projects'>
+			<div className='projects' id='container'>
 				{featured.map((project) => (
 					<FeaturedProjectPreview key={project.title} project={project} />
 				))}
 				<hr />
-				<Slider infinite slidesToShow={1} slidesToScroll={1} speed={500}>
+				<Slider {...sliderProps} ref={sliderRef}>
 					{projects.map((project) => (
 						<Project key={project.id} project={project} />
 					))}
