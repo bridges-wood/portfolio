2a13b5418db629ee6a2e051b99e8654cea49f85e
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-07-13 15:45:22 +0000

    Update styles

---
+++ src/styles/global.ts
@@ -0,0 +1,38 @@
+import { createTheme, globalCss } from '@nextui-org/react'
+
+const globalStyles = globalCss({
+	h1: {
+		fontWeight: 'lighter !important',
+	},
+	'@font-face': [
+		{
+			fontFamily: 'Roboto',
+			src: 'url(https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap)',
+		},
+		{
+			fontFamily: 'Nothing You Could Do',
+			src: ' url(https://fonts.gstatic.com/s/nothingyoucoulddo/v15/oY1B8fbBpaP5OX3DtrRYf_Q2BPB1SnfZb3OOnVs.woff2) format("woff2")',
+		},
+	],
+})
+
+export const darkTheme = createTheme({
+	type: 'dark',
+	theme: {
+		colors: {
+			gradient: 'linear-gradient(45deg, #f5e004, #f02cec)',
+			textGradient: '45deg, #f5e004, #f02cec',
+		},
+	},
+})
+export const lightTheme = createTheme({
+	type: 'light',
+	theme: {
+		colors: {
+			gradient: 'linear-gradient(45deg, #f5e004, #f02cec)',
+			textGradient: '45deg, #f5e004, #f02cec',
+		},
+	},
+})
+
+export default globalStyles
