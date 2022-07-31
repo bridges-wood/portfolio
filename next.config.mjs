56eb5b0a3ed35bb4f88fa1d35de176948455bd46
Max Wood <45072525+bridges-wood@users.noreply.github.com>
2022-07-31 16:09:59 +0000

    Update configs

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+// eslint-disable no-alert
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	i18n: {
+		locales: ['en-GB'],
+		defaultLocale: 'en-GB',
+	},
+	reactStrictMode: true,
+}
+
+export default nextConfig
